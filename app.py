3e3205584a69413bc947924c0dc00ef6ddf26b8e
UndeadAbbi <104169993+UndeadAbbi@users.noreply.github.com>
2024-02-28 19:20:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
@@ -6,6 +7,10 @@ app = Flask(__name__)
 
 model = joblib.load('random_forest_model.joblib')
 scaler = joblib.load('scaler.joblib')
+
+@app.route('/')
+def index():
+    return "Wine Quality Prediction Model"
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -17,4 +22,5 @@ def predict():
         return jsonify({'quality': prediction[0]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
