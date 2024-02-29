e35be3d4ae12b5acc08853ad5d5979d4f03e2980
UndeadAbbi <104169993+UndeadAbbi@users.noreply.github.com>
2024-02-28 19:01:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,12 @@ scaler = joblib.load('scaler.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        feature_values = request.json['features']
+        data = request.get_json(force=True)
         features = np.array(feature_values).reshape(1, -1)
         features_scaled = scaler.transform(features)
         
-        prediction = model.predict(features_scaled)
-        return jsonify({'quality': float(prediction[0])})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+         prediction = model.predict(features_scaled)
+         return jsonify({'quality': prediction[0]})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
